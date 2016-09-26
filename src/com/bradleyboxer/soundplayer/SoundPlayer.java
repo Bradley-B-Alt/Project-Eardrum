1870f3ed2812fe6e85f4ceda06895f93dc6a1fd4
Bradley Boxer <brad.boxer1@gmail.com>
2016-09-26 16:53:40 -0400

    Initial Commit
    Changed code structure to be more versatile and abstract.

---
+++ src/com/bradleyboxer/soundplayer/SoundPlayer.java
@@ -0,0 +1,90 @@
+package com.bradleyboxer.soundplayer;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SoundPlayer extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	static JPanel buttonPanel = new JPanel();
+	static JPanel buttonPanel2 = new JPanel();
+	static JPanel buttonPanel3 = new JPanel();
+	static JButton stopPlaying = new JButton();
+	
+	/*to add sounds, update these lines*/
+	static AudioClip[] clickArray = {simp(0), simp(1), simp(2), simp(3), simp(4), simp(5)};
+	static JButton[] buttonArray = {new JButton(), new JButton(), new JButton(), new JButton(), new JButton(), new JButton()};
+	static String[] buttonNames = {"Explosion", "Rick Astley", "Sandstorm", " Heyeyeyey", "Bork", "FIRST Priority"};
+	/*to add sounds, update these lines*/
+	
+	static int soundTally = clickArray.length;
+	
+	public static AudioClip simp(int resourceNumber) {
+		return Applet.newAudioClip((URL) SoundPlayer.class.getResource("/resources/sound" + String.valueOf(resourceNumber) + ".wav"));
+	}
+	
+	public SoundPlayer() {
+		
+		this.setSize(350, 155);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Zach's Amazing Sound Player");
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		this.setLayout(new FlowLayout());
+		this.add(buttonPanel);
+		this.add(buttonPanel2);
+		this.add(buttonPanel3);
+		this.getContentPane().setBackground(Color.white);
+		this.setResizable(false);
+		
+		for(int i = 0;i<soundTally;i++) { //add every JButton to one of three panels split 3 per
+			if(i<3) {buttonPanel.add(buttonArray[i]);} 
+			if(i<6 && i>=3) {buttonPanel2.add(buttonArray[i]);} 
+			if(i<9 && i>=6) {buttonPanel3.add(buttonArray[i]);} 
+			buttonArray[i].setText(buttonNames[i]); //set text of buttons
+			buttonArray[i].addActionListener(new playSound()); //listen for button presses
+		}
+		
+		buttonPanel3.add(stopPlaying);
+		stopPlaying.setText("Stop Playback");
+		stopPlaying.addActionListener(new stopPlayback());
+	}
+	
+	public static void main(String[] args) {
+		new SoundPlayer(); //main program
+	}
+	
+	public void stopAllSounds() {
+		for(int i = 0;i<soundTally;i++) {
+			clickArray[i].stop();
+		}
+	}
+	
+	private class playSound implements ActionListener {	
+		public void actionPerformed(ActionEvent e) {
+			for(int i = 0;i<soundTally;i++) { //search through buttonArrays and stop/start the clickArray corresponding to it
+				if(e.getSource()==buttonArray[i]) {
+					clickArray[i].stop();
+					clickArray[i].play();
+				}
+			}
+		}
+	}
+	
+	private class stopPlayback implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==stopPlaying) {
+				stopAllSounds();
+			}
+				
+		}
+	}
+}
